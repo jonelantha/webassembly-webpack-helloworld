6f3784fb2af46dfd72670385faf9b1ad1b36457d
jonelantha <57597570+jonelantha@users.noreply.github.com>
2020-04-22 18:32:04 +0100

    Step 2 - Webpack .wat loaders and getHello()

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+import worldGreeterModuleBytes from "./world-greeter.wat";
 import './style.css';
 
 async function main() {
   document.body.innerHTML += `
     <main>
+      <label id='hello'></label>
     </main>
   `;
+
+  const worldGreeter = (
+    await WebAssembly.instantiate(
+      worldGreeterModuleBytes,
+    )
+  ).instance;
+
+  document.getElementById('hello').textContent
+    = worldGreeter.exports.getHello();
 }
 
 main()
