b4fff9e7617b60316b2abc7b53a5aaf578ea7e52
jonelantha <57597570+jonelantha@users.noreply.github.com>
2020-04-24 19:17:41 +0100

    Step 3 - outputWorld()

--- src/index.js
+++ src/index.js
@@ -5,17 +5,31 @@ async function main() {
   document.body.innerHTML += `
     <main>
       <label id='hello'></label>
+      <label id='world'></label>
     </main>
   `;
+
+  const sharedMemory = new WebAssembly.Memory({ initial: 1 });
+
+  const worldCallback = (offset, length) => {
+    const rawBytes = sharedMemory.buffer.slice(offset, length);
+
+    const jsString = new TextDecoder().decode(rawBytes);
+
+    document.getElementById('world').textContent = jsString;
+  }
 
   const worldGreeter = (
     await WebAssembly.instantiate(
       worldGreeterModuleBytes,
+      { imports: { sharedMemory, worldCallback } },
     )
   ).instance;
 
   document.getElementById('hello').textContent
     = worldGreeter.exports.getHello();
+
+  worldGreeter.exports.outputWorld();
 }
 
 main()
